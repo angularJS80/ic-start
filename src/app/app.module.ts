290fce92ae604f6e988815be004b33848b53dac8
angularjs80 <bigcen@naver.com>
2018-01-06 17:26:50 +0900

    bletooth javascript 연동

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,20 @@ import {AngularFireModule} from "angularfire2";
 import {AngularFireAuthModule, AngularFireAuth} from "angularfire2/auth";
 import {Ionic2MaskDirective} from "ionic2-mask-directive";
 import {LoggedinPage} from "../pages/loggedin/loggedin";
+import { BLE } from '@ionic-native/ble';
+
 import * as firebase from 'firebase';
+import {BlefnlistPage} from "../pages/blefnlist/blefnlist";
+import {BleeqlistPage} from "../pages/bleeqlist/bleeqlist";
+
 // Initialize Firebase
 const firebaseAuth= {
-
+  apiKey: "AIzaSyCVovQE5tMaatFfj98oyyjqeaVpoQzqPXA",
+  authDomain: "tutoralnavi3.firebaseapp.com",
+  databaseURL: "https://tutoralnavi3.firebaseio.com",
+  projectId: "tutoralnavi3",
+  storageBucket: "tutoralnavi3.appspot.com",
+  messagingSenderId: "188830952888"
 };
 
 
@@ -28,7 +38,9 @@ const firebaseAuth= {
     LoginPage,
     RegisterPage,
     LoggedinPage,
-    Ionic2MaskDirective
+    Ionic2MaskDirective,
+    BlefnlistPage,
+    BleeqlistPage,
   ],
   imports: [
     BrowserModule,
@@ -42,13 +54,16 @@ const firebaseAuth= {
     HomePage,
     LoginPage,
     RegisterPage,
-    LoggedinPage
+    LoggedinPage,
+    BlefnlistPage,
+    BleeqlistPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     AngularFireAuth,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    BLE
 
     ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, NgZone} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {LoginPage} from "../login/login";
 import {RegisterPage} from "../register/register";
+import {BLE} from "@ionic-native/ble";
+import {BleeqlistPage} from "../bleeqlist/bleeqlist";
+
 
 
 @Component({
@@ -10,9 +13,55 @@ import {RegisterPage} from "../register/register";
 
 })
 export class HomePage {
+
+  Utf8ArrayToStr(arrayBuffer:ArrayBuffer ,service_uuid:string ,characteristic_uuid:string) {
+    console.log("#######decode start arrayBuffer : ");
+    console.log(arrayBuffer );
+    console.log("#######decode start service_uuid : "+service_uuid+"characteristic_uuid : "+characteristic_uuid);
+
+
+  var out, i, len, c;
+  var char2, char3;
+  var array = new Uint32Array(arrayBuffer, 6);
+    console.log("Uint8Array(arrayBuffer)");
+  console.log(array);
+  out = "";
+  len = array.length;
+  i = 0;
+  while(i < len) {
+    c = array[i++];
+    switch(c >> 4)
+    {
+      case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+      // 0xxxxxxx
+      out += String.fromCharCode(c);
+      break;
+      case 12: case 13:
+      // 110x xxxx   10xx xxxx
+      char2 = array[i++];
+      out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+      break;
+      case 14:
+        // 1110 xxxx  10xx xxxx  10xx xxxx
+        char2 = array[i++];
+        char3 = array[i++];
+        out += String.fromCharCode(((c & 0x0F) << 12) |
+          ((char2 & 0x3F) << 6) |
+          ((char3 & 0x3F) << 0));
+        break;
+    }
+  }
+    console.log("#######decode result : "+out);
+
+  return out;
+}
+
+
   constructor(public navCtrl: NavController) {
 
   }
+
+
 
   moveSignIn(){
     this.navCtrl.push(LoginPage);
@@ -21,6 +70,10 @@ export class HomePage {
 
   moveRegister(){
     this.navCtrl.push(RegisterPage)
+  }
+
+  moveBleeqlist(){
+    this.navCtrl.push(BleeqlistPage)
   }
 
 }
